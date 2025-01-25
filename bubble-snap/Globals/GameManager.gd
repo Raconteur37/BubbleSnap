8e79e2e43b45df779b385eb361573d9ce4f6bbdc
Emrie <69336066+Raconteur37@users.noreply.github.com>
2025-01-25 17:15:07 -0600

    Added timer

--- bubble-snap/Globals/GameManager.gd
+++ bubble-snap/Globals/GameManager.gd
@@ -2,5 +2,21 @@ extends Node
 
 var wave = 0
 
-var mainTimer = get_tree().current_scene.find_child("mainTimer")
-var timerLabel = get_tree().current_scene.find_child("timerLabel")
+var inWave = false
+
+var mainTimer
+var timerLabel
+
+func calculateTime():
+	return 60
+
+func startGame():
+	mainTimer = get_tree().current_scene.find_child("mainTimer")
+	timerLabel = get_tree().current_scene.find_child("timerLabel")
+	mainTimer.wait_time = calculateTime()
+	mainTimer.start()
+	inWave = true
+	
+func _process(delta: float) -> void:
+	if inWave:
+		timerLabel.text = str("%.2f" % mainTimer.time_left)
