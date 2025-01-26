cd76f3a906dca2cf34296903158f3a559724dc06
Emrie <69336066+Raconteur37@users.noreply.github.com>
2025-01-25 22:08:00 -0600

    Made half time boon

--- bubble-snap/Bursts/double_time_burst.gd
+++ bubble-snap/Bursts/double_time_burst.gd
@@ -1,19 +1,26 @@
 extends PanelContainer
 
+var animationPlayer
+
 var active = false
 
 var activateType = ["None"]
 
-var currentWave = GameManager.wave
+var currentWave = -1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	BoonManager.addBurst(self)
+	animationPlayer = $AnimationPlayer
 
 func _process(delta: float) -> void:
 	if not active and GameManager.inWave:
-		if GameManager.totalTime / 2 <= GameManager.mainTimer.time_left and GameManager.wave != currentWave:
+		if GameManager.totalTime / 2 >= GameManager.mainTimer.time_left and GameManager.wave != currentWave:
 			active = true
+			animationPlayer.play("Shake")
 			currentWave = GameManager.wave
-			GameManager.mainTimer.wait_time = GameManager.mainTimer.time_left / 2
+			GameManager.mainTimer.stop()
+			GameManager.mainTimer.wait_time = (GameManager.mainTimer.wait_time / 2) / 2
+			GameManager.mainTimer.start()
+			
 	if GameManager.wave != currentWave:
 		active = false
