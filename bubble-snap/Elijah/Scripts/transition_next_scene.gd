65633ff03a6dd84db2f41b3698394c1f83197454
sawl-an-dawnen <78926681+sawl-an-dawnen@users.noreply.github.com>
2025-01-26 03:00:19 -0600

    Game Over fade in and out

--- bubble-snap/Elijah/Scripts/transition_next_scene.gd
+++ bubble-snap/Elijah/Scripts/transition_next_scene.gd
@@ -4,23 +4,13 @@ extends Node
 @export var next_scene_path: String = "res://Elijah/TestEnvironment/MainGameScene.tscn"
 
 @export var transition_anim: AnimationPlayer
-	
-func _on_start_button_pressed() -> void:
-	print(transition_anim)
-	GameManager.resetGame()
-	transition_anim.play("FadeToBlack")
-	
-	await transition_anim.animation_finished
-	
-	#get_tree().change_scene_to_file(next_scene_path)
-	
-	queue_free()
 
+func _on_start_button_pressed() -> void:
+	transition_anim.play("FadeToBlack")
+	await transition_anim.animation_finished
+	get_tree().change_scene_to_file(next_scene_path)
 
 func _on_quit_pressed() -> void:
-	print(transition_anim)
 	transition_anim.play("FadeToBlack")
-	
 	await transition_anim.animation_finished
-	
 	get_tree().quit()
