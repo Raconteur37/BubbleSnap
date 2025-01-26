ef8329c610513abadd3a9191bb957635f15c2ec6
sawl-an-dawnen <78926681+sawl-an-dawnen@users.noreply.github.com>
2025-01-25 18:07:42 -0600

    Main Menu transition
    -black
    -fade animation
    -start button

--- bubble-snap/Elijah/start.gd
+++ bubble-snap/Elijah/start.gd
@@ -3,5 +3,14 @@ extends Node
 # Path to the next scene
 @export var next_scene_path: String = "res://Game.tscn"
 
+var transition_anim: AnimationPlayer
+
+func _ready():
+	transition_anim = $"../../TextureRect/AnimationPlayer"
+
 func _on_pressed() -> void:
+	transition_anim.play("FadeToBlack")
+	
+	await transition_anim.animation_finished
+	
 	get_tree().change_scene_to_file(next_scene_path)
