09d1f93affdce32ba618b442ebd3de0b479eac83
sawl-an-dawnen <78926681+sawl-an-dawnen@users.noreply.github.com>
2025-01-25 21:31:56 -0600

    Update Scene Architecture
    -Main Menu
    -Welcome
    -Game Over

--- bubble-snap/Elijah/TestEnvironment/fadeIn.gd
+++ bubble-snap/Elijah/TestEnvironment/fadeIn.gd
@@ -1,9 +1,4 @@
 extends TextureRect
 
-var transition_anim: AnimationPlayer
-
-func _ready():
-	transition_anim = $AnimationPlayer
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished() -> void:
 	queue_free()
