444270e98ac43edf01e42b6fef49918b28a9c323
Emrie <69336066+Raconteur37@users.noreply.github.com>
2025-01-25 23:46:26 -0600

    Made another boon

--- bubble-snap/Boons/extra_pop.gd
+++ bubble-snap/Boons/extra_pop.gd
@@ -1,11 +1,21 @@
 extends PanelContainer
 
+const boonName = "Extra Hand"
+const description = "Every [color=green]hand pop[/color] has a [color=red]5[/color]% chance to [color=green]hand pop[/color] an extra bubble"
+const icon = "res://Boon Textures/bonusPop.png"
 
-# Called when the node enters the scene tree for the first time.
+var activateChance = 5
+
+var activateType = "Player Pop"
+var rarity = "Common"
+
 func _ready() -> void:
-	pass # Replace with function body.
+	BoonManager.activeBoons.append(self)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func activate():
+	var val = randi_range(0,100)
+	if val <= activateChance:
+		var randBubble = BubbleManager.unPoppedBubbleArray.pick_random()
+		if randBubble != null:
+			randBubble.pop("Player Pop")
+			BubbleManager.addPop(randBubble,"Player Pop")
