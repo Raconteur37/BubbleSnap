a6587da6042dc8a7e523a42d6cc6a24feb5a2e06
sawl-an-dawnen <78926681+sawl-an-dawnen@users.noreply.github.com>
2025-01-26 12:38:05 -0600

    Game Manager Update for reset

--- bubble-snap/Elijah/next_wave_button.gd
+++ bubble-snap/Elijah/next_wave_button.gd
@@ -1,6 +1,10 @@
 extends TextureButton
 
-
 func _on_pressed() -> void:
-	if not GameManager.inWave:
+	if not GameManager.inWave and GameManager.started:
+		print("next")
 		GameManager.nextWave()
+	else:
+		print("start")
+		BubbleBuilder.buildBubbles()
+		GameManager.startGame()
--- bubble-snap/Globals/GameManager.gd
+++ bubble-snap/Globals/GameManager.gd
@@ -14,8 +14,13 @@ var timerLabel
 var waveLabel
 var nextWaveButton
 var popLabel
+var started = false;
 
-var totalTime = 60
+var totalTime = 20
+var totalTimeDefault = 20;
+
+func _ready():
+	totalTimeDefault = totalTime;
 
 func startGame():
 	mainTimer = get_tree().current_scene.find_child("mainTimer")
@@ -23,15 +28,12 @@ func startGame():
 	waveLabel = get_tree().current_scene.find_child("waveLabel")
 	nextWaveButton = get_tree().current_scene.find_child("nextWaveButton")
 	popLabel = get_tree().current_scene.find_child("PopsLabel")
+	print(mainTimer)
+	totalTime = totalTimeDefault
 	mainTimer.wait_time = totalTime
 	mainTimer.start()
 	inWave = true
-	
-func endWave():
-	inWave = false
-	mainTimer.stop()
-	get_tree().current_scene.find_child("CanvasLayer").add_child(load("res://Cameron/BoonSelection.tscn").instantiate())
-	nextWaveButton.show()
+	started = true
 
 func nextWave():
 	mainTimer.wait_time = totalTime
@@ -41,32 +43,55 @@ func nextWave():
 	currentPops = 0
 	inWave = true
 	
+func endWave():
+	nextWaveButton.find_child("Label").text = "NEXT WAVE"
+	inWave = false
+	mainTimer.stop()
+	get_tree().current_scene.find_child("CanvasLayer").add_child(load("res://Cameron/BoonSelection.tscn").instantiate())
+	nextWaveButton.show()
+	
 func resetGame():
+	'''
 	print("got here")
 	atStart = true
 	inWave = false
 	wave = 0
-	#mainTimer.stop()
+	mainTimer.stop()
 	mainTimer.wait_time = totalTime
-	currentPops = 0
 	waveLabel.text = "Wave: " + str(wave)
 	timerLabel.text = str("%.2f" % mainTimer.time_left)
 	popLabel.text = "Pops Needed: " + str(currentPops) + "/" + str(totalPopsNeeded)
 	BubbleManager.resetBubbles()
 	BoonManager.resetBoons()
+	'''
+	get_tree().change_scene_to_file("res://Elijah/Test.tscn")
+	BubbleManager.resetBubbles()
+	BoonManager.resetBoons()
+	wave = 0
+	atStart = true
+	inWave = false
+	gameLost = false
+	started = false
+	currentPops = 0
+	mainTimer.stop()
+	mainTimer.wait_time = totalTime
+	waveLabel.text = "Wave: " + str(wave)
+	timerLabel.text = str("%.2f" % mainTimer.time_left)
+	popLabel.text = "Pops Needed: " + str(currentPops) + "/" + str(totalPopsNeeded)
 	
 
 func _process(delta: float) -> void:
-	if currentPops <= totalPopsNeeded and inWave and not gameLost:
-		nextWaveButton.hide()
-		waveLabel.text = "Wave: " + str(wave)
-		timerLabel.text = str("%.2f" % mainTimer.time_left)
-		popLabel.text = "Pops Needed: " + str(currentPops) + "/" + str(totalPopsNeeded)
-	if mainTimer != null:
-		if mainTimer.time_left <= .02 and inWave and not gameLost:
-			gameLost = true
-			mainTimer.stop()
-			inWave = false
-			get_tree().current_scene.find_child("CanvasLayer").add_child(load("res://Elijah/Scenes/GameOverScene.tscn").instantiate())
-	if currentPops >= totalPopsNeeded and inWave and not gameLost:
-		endWave()
+	if started:
+		if currentPops <= totalPopsNeeded and inWave and not gameLost:
+			nextWaveButton.hide()
+			waveLabel.text = "Wave: " + str(wave)
+			timerLabel.text = str("%.2f" % mainTimer.time_left)
+			popLabel.text = "Pops Needed: " + str(currentPops) + "/" + str(totalPopsNeeded)
+		if mainTimer != null:
+			if mainTimer.time_left <= .02 and inWave and not gameLost:
+				gameLost = true
+				mainTimer.stop()
+				inWave = false
+				get_tree().current_scene.find_child("CanvasLayer").add_child(load("res://Elijah/Scenes/GameOverScene.tscn").instantiate())
+		if currentPops >= totalPopsNeeded and inWave and not gameLost:
+			endWave()
